619cfb909b86f003dc6ab19846af0ebbc6e55a11
Ray Gesualdo <ray@raygesualdo.com>
2022-01-24 14:56:56 -0500

    clean up some meta data

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,7 @@ export function links() {
 }
 
 export const meta: MetaFunction = () => {
-  return { title: 'New Remix App' }
+  return { title: 'Did Ray write this week?' }
 }
 
 export default function App() {
